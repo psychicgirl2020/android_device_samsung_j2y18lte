9fb10cb6e0de7ec319256bc0ea49d477c1974a64
[psychicgirl2020] <[anairachu@gmail.com]>
2020-10-20 09:38:57 +0000

    BoardConfig update

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,51 +4,64 @@ CM_PLATFORM_SDK_VERSION := 7	# Required for libf2fs.so
 #TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/cryptfs_hw	# Lineage
 override TARGET_OUT_VENDOR_SHARED_LIBRARIES = $(TARGET_OUT_SHARED_LIBRARIES)
 
+# For building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES :=true
+
 # Bootloader
 TARGET_NO_BOOTLOADER := true
 TARGET_BOOTLOADER_BOARD_NAME := MSM8937
 
-# Platform
-TARGET_BOARD_PLATFORM := apq8084
-TARGET_BOARD_PLATFORM_GPU := qcom-adreno420
-
 # Flags
-TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+# TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+# TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 #COMMON_GLOBAL_CFLAGS +=
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_PLATFORM := msm8937
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno308
+TARGET_BOARD_SUFFIX := _64
 
 # Kernel
-#TARGET_KERNEL_SOURCE := kernel/samsung/j2718lte
-#TARGET_KERNEL_ARCH := arm
-#TARGET_KERNEL_HEADER_ARCH := arm
-#TARGET_KERNEL_CONFIG := twrp_defconfig
-#TARGET_KERNEL_VARIANT_CONFIG := msm8937_sec_j2y18lte_skt_defconfig
-#KERNEL_TOOLCHAIN_PREFIX := arm-linux-androideabi-
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --second_offset 0x00f00000 --tags_offset 0x01e00000
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_KERNEL_SEPARATED_DT := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
+TARGET_KERNEL_CONFIG := j2y18lte_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # Boot image
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive
-BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_PAGESIZE := 2048
-#BOARD_KERNEL_SEPARATED_DT := true
+# BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive BOARD_KERNEL_BASE := 0x80000000 BOARD_KERNEL_PAGESIZE := 2048
+# BOARD_KERNEL_SEPARATED_DT := true
 # 000RU = recovery kernel, 000KU = system kernel
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --tags_offset 0x01e00000 --board SRPQH31A000RU  --dt device/samsung/j2y18lte/dt.img
+# BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02000000 --second_offset 0x00f00000 --tags_offset 0x01e00000 --board MSM8937  --dt device/samsung/j2y18lte/dt.img
 #BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_TREE)/mkbootimg.mk
+# TARGET_KERNEL_ARCH := arm64
+# TARGET_KERNEL_HEADER_ARCH := arm64
 
 # Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x000D00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x002000000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x09C400000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x2EDAF4000 # 0x2EDAF8000 - 16384 (footer)
-BOARD_FLASH_BLOCK_SIZE := 0x40000
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 3514826752
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 11643367424 # 0x2EDAF8000 - 16384 (footer)
+BOARD_FLASH_BLOCK_SIZE := 131072
 
 # File systems
 BOARD_HAS_LARGE_FILESYSTEM := true
@@ -66,3 +79,4 @@ TW_MAX_BRIGHTNESS := 255
 TW_DEFAULT_BRIGHTNESS := 162
 TW_NO_REBOOT_BOOTLOADER := true
 TW_HAS_DOWNLOAD_MODE := true
+TW_DEVICE_VERSION := PsychicGirl
